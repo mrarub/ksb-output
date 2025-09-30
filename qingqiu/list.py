768debae51afee4a8b252df5a1f2ce4e6ad4faa0
mrarub <mrarub@qq.com>
2025-09-30 17:06:00 +0800

    Update list.py
    更改requests数据获取逻辑

--- qingqiu/list.py
+++ qingqiu/list.py
@@ -1,7 +1,8 @@
 import requests
 import os
+
 def load_config_headers():
-    config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'token.txt')
+    config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'list.txt')
     with open(config_path, 'r', encoding='utf-8') as f:
         content = f.read()
     
@@ -18,11 +19,7 @@ url = "https://api.yisouti.com/user/paper/list"
 base_headers = load_config_headers()
 
 headers = base_headers.copy()
-headers.update({
-  "SIGN": "6680f4a0c503bba45940b3f16660a9800f2bb94e11224741b80b0cc7eeaaacb9",
-  "request-id": "542bb049-1d1d-4205-9ad2-3879f15bbd20",
-  "TIMESTAMP": "1758708302027"
-})
+
 data = """{"size":"20","page":"1"}"""
 
 res = requests.post(url, headers=headers, data=data)
